acddc5f825689c161acdc9fa4a435962faae74a6
adela miclea <adelamiclea709@yahoo.com>
2023-10-19 19:47:53 +0300

    wowwwsfd

---
+++ Controllers/Haine.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Miclea_Adela_laboratorul1.Controllers
+{
+    public class Haine : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit! Vă rugăm să introduceți numele dvs.";
+        }
+
+        public string Salut(string numeUtilizator)
+        {
+            return $"Salut, {numeUtilizator}!";
+        }
+
+        public void AfiseazaMesaj(string mesaj, int numar)
+        {
+            Console.WriteLine($"{mesaj}: {numar}");
+        }
+    }
+}
